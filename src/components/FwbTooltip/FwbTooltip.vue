d39c4ccca296b2cfb27cd1fb6cfa692e47ffe43e
Marcos Noris <marcos.noris26@gmail.com>
2024-06-26 18:08:00 +0200

    tooltip fully working

---
+++ src/components/FwbTooltip/FwbTooltip.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="flex items-start" :class="expanded ? 'w-full' : ''">
+    <tooltip
+      ref="tool_ref"
+      :class="expanded ? 'w-full' : ''"
+      :placement="placement"
+      :triggers="[trigger]"
+      theme="default"
+      :disabled="!active ? true : false"
+      :distance="10"
+    >
+      <slot name="trigger" :class="expanded ? 'w-full' : ''" />
+      <template #popper>
+        <slot v-if="active" name="content" />
+      </template>
+    </tooltip>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { computed } from "vue";
+import { Tooltip } from "floating-vue";
+import type { TooltipPlacement, TooltipStyle, TooltipTrigger } from "./types";
+import "floating-vue/dist/style.css";
+
+interface TooltipProps {
+  placement?: TooltipPlacement;
+ 
+  trigger?: TooltipTrigger;
+  active?: boolean;
+  expanded?: boolean;
+}
+const props = withDefaults(defineProps<TooltipProps>(), {
+  placement: "top",
+ 
+  trigger: "hover",
+  active: true,
+  expanded: false,
+});
+ 
+</script>
+
+<style>
+.v-popper--theme-default .v-popper__wrapper .v-popper__inner {
+  @apply  border-neutral-200 dark:border-neutral-600 rounded-md pt-[6px] pb-[7px] pr-[8px] pl-[6px]  bg-white dark:bg-neutral-700 shadow-md border-[1px];
+}
+
+.v-popper--theme-default .v-popper__arrow-inner {
+  visibility: visible;
+  @apply dark:border-neutral-700 border-white;
+}
+
+ 
+  .v-popper--theme-default .v-popper__arrow-outer {
+    visibility: visible;
+    @apply dark:border-neutral-600 border-neutral-200;
+  }
+
+
+ 
+ 
+
+</style>
+
+ 
+
+
+
+
+ 
---
+++ src/stories/FwbTooltip.stories.ts
@@ -0,0 +1,165 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+ 
+ 
+import FwbTooltip from '@/components/FwbTooltip/FwbTooltip.vue'
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+const meta = {
+  title: 'Example/FwbTooltip',
+  component: FwbTooltip,
+  // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  argTypes: { 
+    trigger: { control: 'select', options: ['hover', 'click' ] },
+    placement: { control: 'select', options: [, "auto"
+        , "auto-start"
+        , "auto-end"
+        , "top"
+        , "top-start"
+        , "top-end"
+        , "bottom"
+        , "bottom-start"
+        , "bottom-end"
+        , "right"
+        , "right-start"
+        , "right-end"
+        , "left"
+        , "left-start"
+        , "left-end"] }
+  },
+  args: {}
+} satisfies Meta<typeof FwbTooltip>
+
+export default meta
+type Story = StoryObj<typeof meta>
+/*
+ *👇 Render functions are a framework specific feature to allow you control on how the component renders.
+ * See https://storybook.js.org/docs/api/csf
+ * to learn how to use render functions.
+ */
+
+
+export const BootomHover: Story = {
+    render: (args) => ({
+      components: { FwbTooltip },
+      setup() {
+        return { args }
+      },
+      template: `<div class="w-full pt-[200px] flex items-center justify-center">
+        <FwbTooltip v-bind="args">        
+          <template #trigger>
+            <div class="w-[90px] h-[35px] pl-3 items-center bg-lime-500 flex rounded-md text-neutral-500 cursor-pointer" >
+                <span >Hover Me<span>
+            </div>
+          </template>       
+          <template #content>
+            <span class="text-sm font-semibold leading-tight text-neutral-900 dark:text-white whitespace-nowrap">Bottom placement</span>
+          </template> 
+        </FwbTooltip>
+      </div>
+      `
+    }),
+    args: {
+      placement: 'bottom'
+    }
+  }
+  export const topHover: Story = {
+    render: (args) => ({
+      components: { FwbTooltip },
+      setup() {
+        return { args }
+      },
+      template: `<div class="w-full pt-[200px] flex items-center justify-center">
+        <FwbTooltip v-bind="args">        
+          <template #trigger>
+            <div class="w-[90px] h-[35px] pl-3 items-center bg-lime-500 flex rounded-md text-neutral-500 cursor-pointer" >
+                <span >Hover Me<span>
+            </div>
+          </template>       
+          <template #content>
+            <span class="text-sm font-semibold leading-tight text-neutral-900 dark:text-white whitespace-nowrap">Top placement</span>
+          </template> 
+        </FwbTooltip>
+      </div>
+      `
+    }),
+    args: {
+      placement: 'top'
+    }
+  }
+  
+  export const leftHover: Story = {
+    render: (args) => ({
+      components: { FwbTooltip },
+      setup() {
+        return { args }
+      },
+      template: `<div class="w-full pt-[200px] flex items-center justify-center">
+        <FwbTooltip v-bind="args">        
+          <template #trigger>
+            <div class="w-[90px] h-[35px] pl-3 items-center bg-lime-500 flex rounded-md text-neutral-500 cursor-pointer" >
+                <span >Hover Me<span>
+            </div>
+          </template>       
+          <template #content>
+            <span class="text-sm font-semibold leading-tight text-neutral-900 dark:text-white whitespace-nowrap">Left placement</span>
+          </template> 
+        </FwbTooltip>
+      </div>
+      `
+    }),
+    args: {
+      placement: 'left'
+    }
+  }
+  
+  export const rightHover: Story = {
+    render: (args) => ({
+      components: { FwbTooltip },
+      setup() {
+        return { args }
+      },
+      template: `<div class="w-full pt-[200px] flex items-center justify-center">
+        <FwbTooltip v-bind="args">        
+          <template #trigger>
+            <div class="w-[90px] h-[35px] pl-3 items-center bg-lime-500 flex rounded-md text-neutral-500 cursor-pointer" >
+                <span >Hover Me<span>
+            </div>
+          </template>       
+          <template #content>
+            <span class="text-sm font-semibold leading-tight text-neutral-900 dark:text-white whitespace-nowrap">Right placement</span>
+          </template> 
+        </FwbTooltip>
+      </div>
+      `
+    }),
+    args: {
+      placement: 'right'
+    }
+  }
+
+  export const topClickAction: Story = {
+    render: (args) => ({
+      components: { FwbTooltip },
+      setup() {
+        return { args }
+      },
+      template: `<div class="w-full pt-[200px] flex items-center justify-center">
+        <FwbTooltip v-bind="args">        
+          <template #trigger>
+            <div class="w-[90px] h-[35px] pl-3 items-center bg-lime-500 flex rounded-md text-neutral-500 cursor-pointer" >
+                <span >Hover Me<span>
+            </div>
+          </template>       
+          <template #content>
+            <span class="text-sm font-semibold leading-tight text-neutral-900 dark:text-white whitespace-nowrap">Right placement</span>
+          </template> 
+        </FwbTooltip>
+      </div>
+      `
+    }),
+    args: {
+      placement: 'top',
+      trigger: 'click'
+    }
+  }
