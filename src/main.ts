93bacce7d6a774fb377dbeb3dc287a4e329ac1a2
Marcos Noris <marcos.noris26@gmail.com>
2024-06-29 01:07:52 +0200

    input select completed

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import { PerfectScrollbarPlugin } from 'vue3-perfect-scrollbar'
+import 'vue3-perfect-scrollbar'
 import App from './App.vue'
 import router from './router'
 
@@ -10,5 +11,5 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(PerfectScrollbarPlugin)
 app.mount('#app')
