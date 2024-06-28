9bae33e509105a5f24184be3c6e5f5f5adfec2fa
Luis Bauza <lebauzagit@gmail.com>
2024-06-28 17:21:18 -0400

    FwbRadio updated

--- src/components/FwbRadio/composables/useRadioClasses.ts
+++ src/components/FwbRadio/composables/useRadioClasses.ts
@@ -8,7 +8,7 @@ const disabledLabelClasses = 'text-neutral-400 dark:text-neutral-500';
 
 // RADIO
 const defaultRadioClasses =
-  'w-4 h-4 text-red-600 bg-neutral-100 border-neutral-300 rounded focus:ring-red-500 dark:focus:ring-red-600 dark:ring-offset-neutral-800 focus:ring-2 dark:bg-neutral-700 dark:border-neutral-600'
+  'w-4 h-4 text-red-600 bg-neutral-100 border-neutral-300 focus:ring-red-500 dark:focus:ring-red-600 dark:ring-offset-neutral-800 focus:ring-2 dark:bg-neutral-700 dark:border-neutral-600'
 
 const disabledRadioClasses ='cursor-not-allowed';
 
