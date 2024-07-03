0b5adb85356e081387a2c0eff64211205527a3a8
Luis Bauza <lebauzagit@gmail.com>
2024-07-03 15:05:49 -0400

    closeBadge functionality added

--- src/components/FwbBadge/FwbBadge.vue
+++ src/components/FwbBadge/FwbBadge.vue
@@ -1,36 +1,40 @@
 <template>
-  <div v-if="pill" :class="pillClasses">
-    <slot />
-  </div>
-  <div v-else-if="indicator" :class="indicatorClasses" />
-  <div v-else :class="wrapperClasses">
-    <!-- Prefix slot for additional content before the badge text -->
-    <div v-if="$slots.preffix" class="">
-      <slot name="preffix" />
+  <div @closeBadge="showBadge=false"  v-if="showBadge">
+    <div v-if="pill" :class="pillClasses">
+      <slot />
     </div>
-
-    <!-- Button text section -->
-    <span :class="spanClasses">
-      <!-- Main Badge text slot -->
-      <div class="">
-        <slot />
-        <!-- <span v-if="!$slots.default">Badge</span> -->
+    <div v-else-if="indicator" :class="indicatorClasses" />
+    <div v-else :class="wrapperClasses">
+      <!-- Prefix slot for additional content before the badge text -->
+      <div v-if="$slots.preffix" class="">
+        <slot name="preffix" />
       </div>
-    </span>
+      <!-- Button text section -->
+      <span :class="spanClasses">
+        <!-- Main Badge text slot -->
+        <div class="">
+          <slot />
+          <!-- <span v-if="!$slots.default">Badge</span> -->
+        </div>
+      </span>
 
-    <!-- Suffix slot for additional content after the badge text -->
-    <div v-if="$slots.suffix" class="">
-      <slot name="suffix" />
+      <!-- Suffix slot for additional content after the badge text -->
+      <div v-if="$slots.suffix" class="">
+        <button @click="showBadge=false" type="button" class="flex items-center justify-center w-3.5 h-3.5 hover:bg-red-200 dark:hover:bg-red-800 rounded-sm">
+          <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18 17.94 6M18 18 6.06 6"/>
+          </svg>
+        </button>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed, toRefs } from 'vue'
+import { computed, ref, toRefs } from 'vue'
+import type { BadgeSize, BadgeVariant } from './types'
 import { useBadgeClasses } from './composables/useBadgeClasses'
-import type { BadgeSize, BadgeVariant } from './types';
 import { useMergeClasses } from '@/composables/userMergeClasses'
-
 
 interface IBadgeProps {
   indicator: boolean
@@ -43,17 +47,23 @@ interface IBadgeProps {
   href?: string
   tag?: string
 }
+
+const showBadge = ref(true)
+
 const props = withDefaults(defineProps<IBadgeProps>(), {
   indicator: false,
   counter: false,
-  size:'md',
+  size: 'md',
   pill: false,
   square: false,
   href: '',
   color: 'primary',
   class: '',
-  tag: 'a',
+  tag: 'a'
 })
+
+
+
 
 const badgeClasses = computed(() => useBadgeClasses(toRefs(props)))
 
@@ -63,4 +73,3 @@ const indicatorClasses = computed(() => useMergeClasses(badgeClasses.value.indic
 const pillClasses = computed(() => useMergeClasses(badgeClasses.value.pillClasses))
 
 </script>
-
