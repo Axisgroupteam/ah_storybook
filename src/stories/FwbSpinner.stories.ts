b4f1ce80b8335b390e1950f712f883935902dcf6
abbatistam <antoniobbatistam@gmail.com>
2024-07-09 08:33:29 -0400

    implemented spinner stories

---
+++ src/stories/FwbSpinner.stories.ts
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+import { fn } from '@storybook/test'
+import FwbSpinner from '@/components/FwbSpinner/FwbSpinner.vue'
+
+const meta = {
+  title: 'Example/FwbSpinner',
+  component: FwbSpinner,
+  tags: ['autodocs'],
+  argTypes: {},
+  args: {}
+} satisfies Meta<typeof FwbSpinner>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Basic: Story = {
+  args: {}
+}
+
+export const Size: Story = {
+  render: (args) => ({
+    components: { FwbSpinner },
+    setup() {
+      return { args }
+    },
+    template: `
+        <div class="flex justify-center items-center gap-4">
+            <FwbSpinner />
+            <FwbSpinner size="6" />
+            <FwbSpinner size="8" />
+            <FwbSpinner size="10" />
+            <FwbSpinner size="12" />
+        </div>              
+        `
+  }),
+  args: {}
+}
