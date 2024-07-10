73984a5384321098d9b569f87168b938e02495f1
abbatistam <antoniobbatistam@gmail.com>
2024-07-10 12:58:51 -0400

    implemented pagination component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', './node_modules/flowbite/**/*.js'],
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+    './node_modules/flowbite/**/*.js',
+    '/stories/**/*.{vue,js,jsx,ts,tsx}'
+  ],
   darkMode: ['class'],
   theme: {
     extend: {
       colors: ({ colors }) => ({
-      //primary: colors.red,
-      gray: colors.neutral
+        //primary: colors.red,
+        gray: colors.neutral
       })
     }
   },
