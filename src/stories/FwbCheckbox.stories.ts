cf40fbb25c23cfac30b94c5f02c50d8a53d1ffdb
abbatistam <antoniobbatistam@gmail.com>
2024-07-15 13:02:23 -0400

    added checkbox doc

--- src/stories/FwbCheckbox.stories.ts
+++ src/stories/FwbCheckbox.stories.ts
@@ -3,8 +3,13 @@ import { fn } from '@storybook/test'
 import FwbCheckbox from '@/components/FwbCheckbox/FwbCheckbox.vue'
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+/**
+ * Get started with the checkbox component to allow the user to select one or more options in the form of a square box.
+ *
+ * The checkbox component can be used to receive one or more selected options from the user in the form of a square box.
+ */
 const meta = {
-  title: 'Example/FwbCheckbox',
+  title: 'Example/CheckboxX',
   component: FwbCheckbox,
   // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
@@ -23,16 +28,25 @@ type Story = StoryObj<typeof meta>
  * See https://storybook.js.org/docs/api/csf
  * to learn how to use render functions.
  */
-export const Normal: Story = {
+/**
+ * Use this variant of a checkbox element in a checked and unchecked state.
+ */
+export const Default: Story = {
   args: {}
 }
 
+/**
+ * This example can be used for the disabled state of the checkbox component.
+ */
 export const Disabled: Story = {
   args: {
     disabled: true
   }
 }
 
+/**
+ * Use this variant of a checkbox item with a custom background color to contrast in an specific case.
+ */
 export const CustomBackgroundColor: Story = {
   render: (args) => ({
     components: { FwbCheckbox },
