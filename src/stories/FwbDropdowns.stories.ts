332199da2c7001f29fec6eebff5e616681428394
abbatistam <antoniobbatistam@gmail.com>
2024-07-20 11:04:08 -0400

    implemented resize

--- src/stories/FwbDropdowns.stories.ts
+++ src/stories/FwbDropdowns.stories.ts
@@ -15,7 +15,7 @@ const meta = {
     () => ({
       template: `
         <div class="p-4">
-        <div class="w-full flex items-start h-[300px]">        
+        <div class="w-full h-[500px]">        
           <story />
         </div>
         </div>`
@@ -142,24 +142,145 @@ export const Secondary: Story = {
                     </svg>
                     </template>
                 </FwbButton>
-                </template>
-                <div >
-                    <div v-for="i in 7" >
+                </template>                
+                    <ul  >
                     
-                      <div class="px-2  py-2 whitespace-nowrap  flex gap-2 justify-start w-[250px] items-center   hover:bg-neutral-100 hover:dark:bg-neutral-600 cursor-pointer rounded-lg text-neutral-500 hover:text-neutral-900 dark:hover:text-white dark:text-neutral-400 text-sm font-medium" >
+                      <li v-for="i in 7" class="px-2  py-2 whitespace-nowrap  flex gap-2 justify-start w-[250px] items-center   hover:bg-neutral-100 hover:dark:bg-neutral-600 cursor-pointer rounded-lg text-neutral-500 hover:text-neutral-900 dark:hover:text-white dark:text-neutral-400 text-sm font-medium" >
                         <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                         <path fill-rule="evenodd" d="M11.293 3.293a1 1 0 0 1 1.414 0l6 6 2 2a1 1 0 0 1-1.414 1.414L19 12.414V19a2 2 0 0 1-2 2h-3a1 1 0 0 1-1-1v-3h-2v3a1 1 0 0 1-1 1H7a2 2 0 0 1-2-2v-6.586l-.293.293a1 1 0 0 1-1.414-1.414l2-2 6-6Z" clip-rule="evenodd"/>
                         </svg>
-                        {{"Element " + i}}</div>
-                    </div>
+                        {{"Element " + i}}</li>
+                    </ul>
                     
-                </div>
+                </fwbDropdown>
+      `
+  }),
+  args: {
+    placement: 'bottom',
+    type: 'secondary'
+  }
+}
+
+export const ResizablePrimary: Story = {
+  render: (args) => ({
+    components: { FwbDropdown, FwbButton },
+
+    methods: {
+      toggleVisibility(value: boolean): void {
+        this.isVisible = value
+      }
+    },
+    setup() {
+      return { args, isVisible: ref(false) }
+    },
+    template: `
+                <FwbDropdown @toogle="toggleVisibility" v-bind="args">
+              <template #trigger>
+              <FwbButton
+                 size="md"
+                  color="secondary"
+                  class="whitespace-nowrap"
+      
+              >
+                 
+                      Click me
+                 
+                  <template #suffix>
+                  <svg
+                      :class="isVisible ? 'rotate-180' : 'rotate-0'"
+                      class="w-4 h-4 ml-2"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                  >
+                      <path
+                      d="M19 9l-7 7-7-7"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      />
+                  </svg>
+                  </template>
+              </FwbButton>
+              </template>
+              <ul>
+                  <li v-for="i in 7" class="px-4 overflow-x-clip w-[200px] py-4 items-start flex gap-2 border-b whitespace-nowrap border-b-neutral-200 dark:border-b-neutral-600 last:border-b-0 hover:bg-neutral-100 hover:dark:bg-neutral-600 cursor-pointer first:rounded-t-lg justify-start last:rounded-b-lg text-neutral-500 hover:text-neutral-900 dark:hover:text-white dark:text-neutral-400 text-sm font-medium">
+                  <svg class="w-5 h-5"  aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                    <path fill-rule="evenodd" d="M11.293 3.293a1 1 0 0 1 1.414 0l6 6 2 2a1 1 0 0 1-1.414 1.414L19 12.414V19a2 2 0 0 1-2 2h-3a1 1 0 0 1-1-1v-3h-2v3a1 1 0 0 1-1 1H7a2 2 0 0 1-2-2v-6.586l-.293.293a1 1 0 0 1-1.414-1.414l2-2 6-6Z" clip-rule="evenodd"/>
+                  </svg>
+                  <span >{{"Element " + i}}</span>
+                  </li>
+                  
+              </ul>
+              </FwbDropdown>
+    `
+  }),
+  args: {
+    placement: 'bottom',
+    type: 'primary',
+    resizable: true
+  }
+}
+
+export const ResizableSecondary: Story = {
+  render: (args) => ({
+    components: { FwbDropdown, FwbButton },
+
+    methods: {
+      toggleVisibility(value: boolean): void {
+        this.isVisible = value
+      }
+    },
+    setup() {
+      return { args, isVisible: ref(false) }
+    },
+    template: `
+                <fwbDropdown v-bind="args" @toogle="toggleVisibility">
+                <template #trigger>
+                <FwbButton
+                            
+                    color="secondary"
+                    size="md"
+                    class="whitespace-nowrap"
+                >
+                    
+                        Click me
+                    
+                    <template #suffix>
+                    <svg
+                        :class="isVisible ? 'rotate-180' : 'rotate-0'"
+                        class="w-4 h-4 ml-2"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                        d="M19 9l-7 7-7-7"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        />
+                    </svg>
+                    </template>
+                </FwbButton>
+                </template>
+                    <ul  >
+                    
+                      <li v-for="i in 7" class="px-2  py-2 whitespace-nowrap  flex gap-2 justify-start w-[250px] items-center   hover:bg-neutral-100 hover:dark:bg-neutral-600 cursor-pointer rounded-lg text-neutral-500 hover:text-neutral-900 dark:hover:text-white dark:text-neutral-400 text-sm font-medium" >
+                        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                        <path fill-rule="evenodd" d="M11.293 3.293a1 1 0 0 1 1.414 0l6 6 2 2a1 1 0 0 1-1.414 1.414L19 12.414V19a2 2 0 0 1-2 2h-3a1 1 0 0 1-1-1v-3h-2v3a1 1 0 0 1-1 1H7a2 2 0 0 1-2-2v-6.586l-.293.293a1 1 0 0 1-1.414-1.414l2-2 6-6Z" clip-rule="evenodd"/>
+                        </svg>
+                        {{"Element " + i}}</li>
+                    </ul>
+                    
                 </fwbDropdown>
       `
   }),
   args: {
     placement: 'bottom',
     type: 'secondary',
-    max_h: 'max-h-[144px]'
+    resizable: true
   }
 }
