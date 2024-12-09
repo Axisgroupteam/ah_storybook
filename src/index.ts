d04954018b9c5d520fec1d270b929c5bc0586c24
Miguel Verdecia <kyljuru@gmail.com>
2024-12-09 12:55:55 -0500

    working on table styles

--- src/index.ts
+++ src/index.ts
@@ -1,123 +1,121 @@
-// import ButtonX from './components/FwbButton/FwbButton.vue'
-//import AppSelectDropBox from './components/AppSelectDropBox/presentation/AppSelectDropBox.vue'
-import DatePickerX from './components/DatePicker/DatePicker.vue'
-//import Drawer from './components/Drawer/presentation/Drawer.vue'
-import AccordionX from './components/FwbAccordion/FwbAccordion.vue'
-import AccordionContentX from './components/FwbAccordion/FwbAccordionContent.vue'
-import AccordionHeaderX from './components/FwbAccordion/FwbAccordionHeader.vue'
-import AccordionPanelX from './components/FwbAccordion/FwbAccordionPanel.vue'
-import AvatarX from './components/FwbAvatar/FwbAvatar.vue'
-import AvatarStackX from './components/FwbAvatar/FwbAvatarStack.vue'
-import BadgeX from './components/FwbBadge/FwbBadge.vue'
-import BreadcrumbX from './components/FwbBreadcrumb/FwbBreadcrumb.vue'
-import BreadcrumbItemX from './components/FwbBreadcrumb/FwbBreadcrumbItem.vue'
-import ButtonGroupX from './components/FwbButtonGroup/FwbButtonGroup.vue'
-import AreaX from './components/FwbCharts/Area/AreaChart.vue'
-import BarX from './components/FwbCharts/Bar/BarChart.vue'
-import ColumnX from './components/FwbCharts/Column/ColumnChart.vue'
-import DonutX from './components/FwbCharts/Donut/DonutChart.vue'
-import LineX from './components/FwbCharts/Line/LineChart.vue'
-import PieX from './components/FwbCharts/Pie/PieChart.vue'
-import RadialX from './components/FwbCharts/Radial/RadialChart.vue'
-import CheckboxX from './components/FwbCheckbox/FwbCheckbox.vue'
-import DropdownX from './components/FwbDropdown/FwbDropdown.vue'
-import FileInputX from './components/FwbDropzone/FwbFileInput.vue'
-import GalleryX from './components/FwbGallery/FwbGallery.vue'
-import ImageX from './components/FwbImage/FwbImage.vue'
-import IndicatorX from './components/FwbIndicators/FwbIndicator.vue'
-import InputX from './components/FwbInput/FwbInput.vue'
-import InputPasswordX from './components/FwbInput/InputPassword.vue'
-import ModalX from './components/FwbModal/FwbModal.vue'
-//import FwbNavbar from './components/FwbNavbar/FwbNavbar.vue'
-import PaginationX from './components/FwbPagination/FwbPagination.vue'
-import PhoneInputX from './components/FwbPhoneInput/FwbPhoneInput.vue'
-import ProgressX from './components/FwbProgress/FwbProgress.vue'
-import RadioX from './components/FwbRadio/FwbRadio.vue'
-import RangeX from './components/FwbRange/FwbRange.vue'
-import SelectX from './components/FwbSelect/FwbSelect.vue'
-import SkeletonX from './components/FwbSkeleton/FwbSkeleton.vue'
-import SpeedDialX from './components/FwbSpeedDial/FwbSpeedDial.vue'
-import SpinnerX from './components/FwbSpinner/FwbSpinner.vue'
-import StepperX from './components/FwbStepper/FwbStepper.vue'
-import TableX from './components/FwbTable/FwbTable.vue'
-import TableRowX from './components/FwbTable/FwbTableRow.vue'
-import TableCellX from './components/FwbTable/FwbTableCell.vue'
-import TableHeadX from './components/FwbTable/FwbTableHead.vue'
-import TableHeadCellX from './components/FwbTable/FwbTableHeadCell.vue'
-import TableBodyX from './components/FwbTable/FwbTableBody.vue'
-import TabX from './components/FwbTabs/FwbTab.vue'
-import TabPaneX from './components/FwbTabs/FwbTabPane.vue'
-import TabsX from './components/FwbTabs/FwbTabs.vue'
-import TextareaX from './components/FwbTextarea/FwbTextarea.vue'
-import ToggleX from './components/FwbToggle/FwbToggle.vue'
-import TooltipX from './components/FwbTooltip/FwbTooltip.vue'
-import SearchCustomX from './components/FwbInputSearchCustom/FwbInputSearchCustom.vue'
-import FwbChooseQuantity from './components/FwbChooseQuantity/FwbChooseQuantity.vue'
-import IconX from './components/IconWrapper.vue'
-import type { iconsNames } from './components/data/icons.ts'
-export type { iconsNames as IconsNames }
-import ExpandableTableX from './components/FwbTable/NestedTable/ExpandableTable.vue'
 import { defineAsyncComponent } from 'vue'
-const ButtonX = defineAsyncComponent(() => import('./components/FwbButton/FwbButton.vue'))
-// const CarouselX = defineAsyncComponent(() => import('./components/FwbCarousel/FwbCarousel.vue'))
 
-import '@/components/FwbTable/NestedTable/styles/table_styles.css'
-export {
-  ButtonX,
-  DatePickerX,
-  //Drawer,
-  AccordionX,
-  AccordionContentX,
-  AccordionHeaderX,
-  AccordionPanelX,
-  AvatarX,
-  AvatarStackX,
-  BadgeX,
-  BreadcrumbX,
-  BreadcrumbItemX,
-  ButtonGroupX,
-  AreaX,
-  BarX,
-  ColumnX,
-  DonutX,
-  LineX,
-  //FwbNavbar,
-  PieX,
-  RadialX,
-  CheckboxX,
-  DropdownX,
-  FileInputX,
-  GalleryX,
-  ImageX,
-  IndicatorX,
-  InputX,
-  InputPasswordX,
-  ModalX,
-  PaginationX,
-  PhoneInputX,
-  ProgressX,
-  RadioX,
-  RangeX,
-  SelectX,
-  SkeletonX,
-  SpeedDialX,
-  SpinnerX,
-  StepperX,
-  TableX,
-  TableRowX,
-  TableCellX,
-  TableHeadX,
-  TableHeadCellX,
-  TabX,
-  TableBodyX,
-  TabPaneX,
-  TabsX,
-  TextareaX,
-  ToggleX,
-  TooltipX,
-  SearchCustomX,
-  IconX,
-  FwbChooseQuantity,
-  ExpandableTableX
-  // CarouselX
-}
+// export ButtonX from './components/FwbButton/FwbButton.vue'
+//export AppSelectDropBox from './components/AppSelectDropBox/presentation/AppSelectDropBox.vue'
+export const DatePickerX = defineAsyncComponent(
+  () => import('./components/DatePicker/DatePicker.vue')
+)
+//export Drawer from './components/Drawer/presentation/Drawer.vue'
+export const AccordionX = defineAsyncComponent(
+  () => import('./components/FwbAccordion/FwbAccordion.vue')
+)
+export const AccordionContentX = defineAsyncComponent(
+  () => import('./components/FwbAccordion/FwbAccordionContent.vue')
+)
+export const AccordionHeaderX = defineAsyncComponent(
+  () => import('./components/FwbAccordion/FwbAccordionHeader.vue')
+)
+export const AccordionPanelX = defineAsyncComponent(
+  () => import('./components/FwbAccordion/FwbAccordionPanel.vue')
+)
+export const AvatarX = defineAsyncComponent(() => import('./components/FwbAvatar/FwbAvatar.vue'))
+export const AvatarStackX = defineAsyncComponent(
+  () => import('./components/FwbAvatar/FwbAvatarStack.vue')
+)
+export const BadgeX = defineAsyncComponent(() => import('./components/FwbBadge/FwbBadge.vue'))
+export const BreadcrumbX = defineAsyncComponent(
+  () => import('./components/FwbBreadcrumb/FwbBreadcrumb.vue')
+)
+export const BreadcrumbItemX = defineAsyncComponent(
+  () => import('./components/FwbBreadcrumb/FwbBreadcrumbItem.vue')
+)
+export const ButtonGroupX = defineAsyncComponent(
+  () => import('./components/FwbButtonGroup/FwbButtonGroup.vue')
+)
+export const AreaX = defineAsyncComponent(() => import('./components/FwbCharts/Area/AreaChart.vue'))
+export const BarX = defineAsyncComponent(() => import('./components/FwbCharts/Bar/BarChart.vue'))
+export const ColumnX = defineAsyncComponent(
+  () => import('./components/FwbCharts/Column/ColumnChart.vue')
+)
+export const DonutX = defineAsyncComponent(
+  () => import('./components/FwbCharts/Donut/DonutChart.vue')
+)
+export const LineX = defineAsyncComponent(() => import('./components/FwbCharts/Line/LineChart.vue'))
+export const PieX = defineAsyncComponent(() => import('./components/FwbCharts/Pie/PieChart.vue'))
+export const RadialX = defineAsyncComponent(
+  () => import('./components/FwbCharts/Radial/RadialChart.vue')
+)
+export const CheckboxX = defineAsyncComponent(
+  () => import('./components/FwbCheckbox/FwbCheckbox.vue')
+)
+export const DropdownX = defineAsyncComponent(
+  () => import('./components/FwbDropdown/FwbDropdown.vue')
+)
+export const FileInputX = defineAsyncComponent(
+  () => import('./components/FwbDropzone/FwbFileInput.vue')
+)
+export const GalleryX = defineAsyncComponent(() => import('./components/FwbGallery/FwbGallery.vue'))
+export const ImageX = defineAsyncComponent(() => import('./components/FwbImage/FwbImage.vue'))
+export const IndicatorX = defineAsyncComponent(
+  () => import('./components/FwbIndicators/FwbIndicator.vue')
+)
+export const InputX = defineAsyncComponent(() => import('./components/FwbInput/FwbInput.vue'))
+export const InputPasswordX = defineAsyncComponent(
+  () => import('./components/FwbInput/InputPassword.vue')
+)
+export const ModalX = defineAsyncComponent(() => import('./components/FwbModal/FwbModal.vue'))
+//export FwbNavbar from './components/FwbNavbar/FwbNavbar.vue'
+export const PaginationX = defineAsyncComponent(
+  () => import('./components/FwbPagination/FwbPagination.vue')
+)
+export const PhoneInputX = defineAsyncComponent(
+  () => import('./components/FwbPhoneInput/FwbPhoneInput.vue')
+)
+export const ProgressX = defineAsyncComponent(
+  () => import('./components/FwbProgress/FwbProgress.vue')
+)
+export const RadioX = defineAsyncComponent(() => import('./components/FwbRadio/FwbRadio.vue'))
+export const RangeX = defineAsyncComponent(() => import('./components/FwbRange/FwbRange.vue'))
+export const SelectX = defineAsyncComponent(() => import('./components/FwbSelect/FwbSelect.vue'))
+export const SkeletonX = defineAsyncComponent(
+  () => import('./components/FwbSkeleton/FwbSkeleton.vue')
+)
+export const SpeedDialX = defineAsyncComponent(
+  () => import('./components/FwbSpeedDial/FwbSpeedDial.vue')
+)
+export const SpinnerX = defineAsyncComponent(() => import('./components/FwbSpinner/FwbSpinner.vue'))
+export const StepperX = defineAsyncComponent(() => import('./components/FwbStepper/FwbStepper.vue'))
+export const TableX = defineAsyncComponent(() => import('./components/FwbTable/FwbTable.vue'))
+export { default as TableRowX } from './components/FwbTable/FwbTableRow.vue'
+export const TableCellX = defineAsyncComponent(
+  () => import('./components/FwbTable/FwbTableCell.vue')
+)
+export const TableHeadX = defineAsyncComponent(
+  () => import('./components/FwbTable/FwbTableHead.vue')
+)
+export const TableHeadCellX = defineAsyncComponent(
+  () => import('./components/FwbTable/FwbTableHeadCell.vue')
+)
+export const TableBodyX = defineAsyncComponent(
+  () => import('./components/FwbTable/FwbTableBody.vue')
+)
+export const TabX = defineAsyncComponent(() => import('./components/FwbTabs/FwbTab.vue'))
+export const TabPaneX = defineAsyncComponent(() => import('./components/FwbTabs/FwbTabPane.vue'))
+export const TabsX = defineAsyncComponent(() => import('./components/FwbTabs/FwbTabs.vue'))
+export const TextareaX = defineAsyncComponent(
+  () => import('./components/FwbTextarea/FwbTextarea.vue')
+)
+export const ToggleX = defineAsyncComponent(() => import('./components/FwbToggle/FwbToggle.vue'))
+export const TooltipX = defineAsyncComponent(() => import('./components/FwbTooltip/FwbTooltip.vue'))
+export const SearchCustomX = defineAsyncComponent(
+  () => import('./components/FwbInputSearchCustom/FwbInputSearchCustom.vue')
+)
+export const FwbChooseQuantity = defineAsyncComponent(
+  () => import('./components/FwbChooseQuantity/FwbChooseQuantity.vue')
+)
+export const IconX = defineAsyncComponent(() => import('./components/IconWrapper.vue'))
+export type { iconsNames } from './components/data/icons.ts'
+export const ExpandableTableX = defineAsyncComponent(
+  () => import('./components/FwbTable/NestedTable/ExpandableTable.vue')
+)
+export const ButtonX = defineAsyncComponent(() => import('./components/FwbButton/FwbButton.vue'))
